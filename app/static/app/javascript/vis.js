a96f7a26f101b4fa1c72cb17f76c77795853b1ed
James-W-Pettit <108548172+JDumbs@users.noreply.github.com>
2023-01-16 17:44:21 -0600

    added visulizer test page
    ye correct this time

---
+++ app/static/app/javascript/vis.js
@@ -0,0 +1,9 @@
+let audio1 = new Audio();
+audio1.src = "tune.mp3";
+
+const container = document.getElementById("container");
+const canvas = document.getElementById("canvas");
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+const ctx = canvas.getContext("2d");
