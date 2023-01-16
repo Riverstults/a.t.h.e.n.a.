8def5946e7a8af1ede24ef2fa99cc242d6c82075
NuzziVF <76072275+NuzziVF@users.noreply.github.com>
2023-01-15 22:52:32 -0600

    I moved the functions around and commented
    I changed the path to the url so that it works now, I added templates, I commented on things that river needs to change, and I changed the dunder name.

--- app/views.py
+++ app/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-import pyttsx3 
+import pyttsx3
 import speech_recognition as sr
 import datetime
 import wikipedia
@@ -10,49 +10,27 @@ import requests
 from pprint import pprint
 from selenium import webdriver
 
+### all comments with the triple hashtags are me, Eric.
 
-engine = pyttsx3.init('sapi5')
-voices = engine.getProperty('voices')
-#print(voices[1].id)
-engine.setProperty('voice',voices[1].id)
+
+engine = pyttsx3.init("sapi5")
+voices = engine.getProperty("voices")
+# print(voices[1].id)
+engine.setProperty("voice", voices[1].id)
+
+
+def homePage(request):
+    context = {}
+    return render(request, "homePage.html", context)
+
 
 def speak(audio):
     engine.say(audio)
     engine.runAndWait()
 
 
-def wishMe():
-    speak("Welcome back sir")
-    hour = int(datetime.datetime.now().hour)
-    print(hour)
-    year = int(datetime.datetime.now().year)
-    month = int(datetime.datetime.now().month)
-    date = int(datetime.datetime.now().day)
-    Time = datetime.datetime.now().strftime("%I:%M:%S") 
-    print(Time)
-    print(month)
-    print(date)
-    print(year)
-    speak("the current Time is")
-    speak(Time)
-    speak("the current Date is")
-    speak(month)
-    speak(date)
-    speak(year)
-    if hour>=6 and hour<12:
-        speak("Good Morning sir!")
-
-    elif hour>=12 and hour<18:
-        speak("Good Afternoon sir!")
-
-    elif hour>=18 and hour<24:
-        speak("Good Evening sir!")
-
-    else:
-        speak("Good Night sir!")
-
-    speak("athena at your Service. Please tell me how can I help You ")
-#wishMe()
+### What is this comment trying to say??
+# wishMe()
 def takeCommand():
 
     r = sr.Recognizer()
@@ -60,11 +38,11 @@ def takeCommand():
         print("Listening...")
         # r.pause_threshold = 0.7
         audio = r.listen(source, phrase_time_limit=5)
-        print (audio)
+        print(audio)
 
     try:
         print("Recognizing...")
-        query = r.recognize_google(audio, language='en-in')
+        query = r.recognize_google(audio, language="en-in")
         print(f"river Said:{query}\n")
 
     except Exception as e:
@@ -76,13 +54,16 @@ def takeCommand():
 
 
 def sendEmail(to, content):
-    server = smtplib.SMTP('smtp.gmail.com', 587)
+    server = smtplib.SMTP("smtp.gmail.com", 587)
     server.ehlo()
     server.starttls()
-    server.login('Senderemail@gmail.com', 'Password')
-    server.sendmail('Senderemail@gmail.com', to, content)
+    server.login("Senderemail@gmail.com", "Password")
+    server.sendmail("Senderemail@gmail.com", to, content)
     server.close()
 
+
+### Please leave out code that is not being used, comments should be used for exactly that, comments.
+### If this isn't being used, delete it, if it is then uncomment it please.
 # def lighton():
 #     driver = webdriver.Chrome('C:/Users/Username/Downloads/chromedriver.exe')
 #     driver.get("https://Add here.000webhostapp.com/main.html")
@@ -95,68 +76,104 @@ def sendEmail(to, content):
 #     elem1 = driver.find_element_by_id("S1on")
 #     elem1.click()
 
-if __name__ == "__main__":
+
+### can we please use more concise naming schemes, "wishMe" is in the right case but it means nothing to me, I have no clue what it even means or does.
+def wishMe():
+    speak("Welcome back sir")
+    hour = int(datetime.datetime.now().hour)
+    print(hour)
+    year = int(datetime.datetime.now().year)
+    month = int(datetime.datetime.now().month)
+    date = int(datetime.datetime.now().day)
+    Time = datetime.datetime.now().strftime("%I:%M:%S")
+    print(Time)
+    print(month)
+    print(date)
+    print(year)
+    speak("the current Time is")
+    speak(Time)
+    speak("the current Date is")
+    speak(month)
+    speak(date)
+    speak(year)
+    if hour >= 6 and hour < 12:
+        speak("Good Morning sir!")
+
+    elif hour >= 12 and hour < 18:
+        speak("Good Afternoon sir!")
+
+    elif hour >= 18 and hour < 24:
+        speak("Good Evening sir!")
+
+    else:
+        speak("Good Night sir!")
+
+    speak("athena at your Service. Please tell me how can I help You ")
+
+
+### wtf is all of this doing in main, this should be in a function outside of main.
+def main():
     wishMe()
     while True:
         query = takeCommand().lower()
 
-
-        if 'wikipedia' in query:
-            speak('Searching Wikipedia...')
+        if "wikipedia" in query:
+            speak("Searching Wikipedia...")
             query = query.replace("wikipedia", "")
             results = wikipedia.summary(query, sentences=2)
             speak("According to Wikipedia")
             print(results)
             speak(results)
 
-        elif 'search in chrome' in query:
+        elif "search in chrome" in query:
             speak("what should i search?")
-            chrome_path = 'C:/Program Files (x86)/Google/Chrome/Application/chrome.exe %s'
+            chrome_path = (
+                "C:/Program Files (x86)/Google/Chrome/Application/chrome.exe %s"
+            )
 
             r = sr.Recognizer()
 
             with sr.Microphone() as source:
-                print('say something!')
+                print("say something!")
                 audio = r.listen(source)
                 print("done")
             try:
                 text = r.recognize_google(audio)
-                print('google think you said:\n' +text +'.com')
-                wb.get(chrome_path).open(text+'.com')
+                print("google think you said:\n" + text + ".com")
+                wb.get(chrome_path).open(text + ".com")
             except Exception as e:
                 print(e)
-        
-        elif 'how is the weather' and 'weather' in query:
 
-            url = 'http://api.weatherstack.com/current'#Open api link here
-            access_key = 'b1c04b7b8ff04ccea5a9d06ab232b0b1'
+        elif "how is the weather" and "weather" in query:
+
+            url = "http://api.weatherstack.com/current"  # Open api link here
+            access_key = "b1c04b7b8ff04ccea5a9d06ab232b0b1"
 
             res = requests.get(url)
 
             data = res.json()
 
-            weather = data['weather'] [0] ['main'] 
-            temp = data['main']['temp']
-            wind_speed = data['wind']['speed']
+            weather = data["weather"][0]["main"]
+            temp = data["main"]["temp"]
+            wind_speed = data["wind"]["speed"]
 
-            latitude = data['coord']['lat']
-            longitude = data['coord']['lon']
+            latitude = data["coord"]["lat"]
+            longitude = data["coord"]["lon"]
 
-            description = data['weather'][0]['description']
-            speak('Temperature : {} degree celcius'.format(temp))
-            print('Wind Speed : {} m/s'.format(wind_speed))
-            print('Latitude : {}'.format(latitude))
-            print('Longitude : {}'.format(longitude))
-            print('Description : {}'.format(description))
-            print('weather is: {} '.format(weather))
-            speak('weather is : {} '.format(weather))
+            description = data["weather"][0]["description"]
+            speak("Temperature : {} degree celcius".format(temp))
+            print("Wind Speed : {} m/s".format(wind_speed))
+            print("Latitude : {}".format(latitude))
+            print("Longitude : {}".format(longitude))
+            print("Description : {}".format(description))
+            print("weather is: {} ".format(weather))
+            speak("weather is : {} ".format(weather))
 
-
-        elif 'the time' in query:
-            strTime = datetime.datetime.now().strftime("%I:%M:%S")    
+        elif "the time" in query:
+            strTime = datetime.datetime.now().strftime("%I:%M:%S")
             speak(f"Sir, the time is {strTime}")
-        
-        elif 'the date' in query:
+
+        elif "the date" in query:
             year = int(datetime.datetime.now().year)
             month = int(datetime.datetime.now().month)
             date = int(datetime.datetime.now().day)
@@ -165,38 +182,39 @@ if __name__ == "__main__":
             speak(date)
             speak(year)
 
-
-        elif 'send reminder' and 'send email' in query:
+        elif "send reminder" and "send email" in query:
             try:
                 speak("What should I say?")
                 content = takeCommand()
-                to = "pstults04@gmail.com"    
+                to = "pstults04@gmail.com"
                 sendEmail(to, content)
                 speak("Email has been sent!")
             except Exception as e:
                 print(e)
-                speak("Sorry my friend . I am not able to send this email")      
+                speak("Sorry my friend . I am not able to send this email")
 
-        elif 'open' in query:
-            os.system('explorer C://{}'.format(query.replace('Open','')))
+        elif "open" in query:
+            os.system("explorer C://{}".format(query.replace("Open", "")))
 
-        
         # elif 'turn on lights' in query:
         #     speak("OK,sir turning on the Lights")
         #     lighton()
         #     speak("Lights are on")
-        
+
         # elif 'turn off lights' in query:
         #     speak("OK,sir turning off the Lights")
         #     lightoff()
         #     speak("Lights are off")
 
-
-
-        elif 'go offline'  in query:
+        elif "go offline" in query:
             speak("ok sir shutting down the system")
             quit()
 
-        elif 'shutdown'  in query:
+        elif "shutdown" in query:
             speak("ok sir shutting down the system")
             quit()
+
+
+### wtf do we even have a dunder name = dunder main?!?! this is not the place for this
+if __name__ == "__main__":
+    main()
