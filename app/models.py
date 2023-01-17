45d934b6a1038cbd9629147d35306a43514e068a
Riverstults <pstults04@gmail.com>
2023-01-17 01:35:40 -0600

    java

--- app/models.py
+++ app/models.py
@@ -6,6 +6,7 @@ from django.contrib.auth.models import User
 class Person(models.Model):
 	user = models.OneToOneField(User,null = True, on_delete=models.CASCADE)
 	name = models.CharField(max_length=200, null=True)
+
     
 	def __str__(self):
 		return self.name
